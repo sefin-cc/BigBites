53f317404bb1a11616b298b7361c612282c5cbb6
sefin-cc <rogenasefin6@gmail.com>
2025-04-10 20:27:30 +0800

    Update app.php

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -26,20 +26,20 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
     // Define custom middleware group for admin
-    $middleware->group('admin.api', [
-        \App\Http\Middleware\CustomEnsureFrontendRequestsAreStateful::class, 
-        \Illuminate\Cookie\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-    ]);
-    
-    //For local development:
     // $middleware->group('admin.api', [
-    //     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    //     \App\Http\Middleware\CustomEnsureFrontendRequestsAreStateful::class, 
+    //     \Illuminate\Cookie\Middleware\EncryptCookies::class,
+    //     \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
     //     \Illuminate\Session\Middleware\StartSession::class,
     //     \Illuminate\View\Middleware\ShareErrorsFromSession::class,
     // ]);
+    
+
+    $middleware->group('admin.api', [
+        \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    ]);
 
         $middleware->statefulApi();
 
