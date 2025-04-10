35f141bac072e70e5f252d69c45243960786158d
sefin-cc <rogenasefin6@gmail.com>
2025-04-10 14:34:34 +0800

    Update app.php

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -27,10 +27,13 @@ return Application::configure(basePath: dirname(__DIR__))
 
     // Define custom middleware group for admin
     $middleware->group('admin.api', [
-        \App\Http\Middleware\CustomEnsureFrontendRequestsAreStateful::class,
+        \App\Http\Middleware\CustomEnsureFrontendRequestsAreStateful::class, 
+        \Illuminate\Cookie\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
     ]);
+    
 
         $middleware->statefulApi();
 
