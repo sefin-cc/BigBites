c010810cf1fe84c533f11e36600dd9cf00df4dc9
sefin-cc <rogenasefin6@gmail.com>
2025-04-11 15:59:07 +0800

    Update pusher.ts

--- admin/src/utils/pusher.ts
+++ admin/src/utils/pusher.ts
@@ -1,33 +1,32 @@
 import Pusher, { Options } from "pusher-js";
 
 const PUSHER_APP_KEY = import.meta.env.VITE_PUSHER_APP_KEY as string;
-const PUSHER_CLUSTER = import.meta.env.VITE_PUSHER_CLUSTER as string; // Required for TypeScript
-
 const REVERB_HOST = import.meta.env.VITE_REVERB_HOST as string;
 const REVERB_PORT = Number(import.meta.env.VITE_REVERB_PORT);
+const REVERB_SCHEME = import.meta.env.VITE_REVERB_SCHEME;
+const PUSHER_CLUSTER = import.meta.env.VITE_PUSHER_CLUSTER as string; 
 
 // Define Pusher options
 const options: Options = {
-  cluster: PUSHER_CLUSTER, // Required to avoid TypeScript error
-  wsHost: REVERB_HOST, // Localhost WebSocket Server
-  wsPort: REVERB_PORT, // Laravel Reverb WebSocket Port
-  forceTLS: false, // No TLS for localhost
-  disableStats: true, // Disable Pusher stats
-  enabledTransports: ["ws"], // Use WebSockets only
+  wsHost: REVERB_HOST,
+  wsPort: REVERB_PORT,
+  wssPort: REVERB_PORT,
+  forceTLS: REVERB_SCHEME === "https",
+  disableStats: true,
+  enabledTransports: ["ws", "wss"],
+  cluster: PUSHER_CLUSTER,
 };
 
-// Initialize Pusher with cluster & Reverb settings
+// Initialize Pusher for Laravel Reverb
 const pusher = new Pusher(PUSHER_APP_KEY, options);
 
-// Subscribe to the "orders" channel
+// Subscribe to channels and events
 const channel = pusher.subscribe("orders");
-
-// Listen for the "OrderUpdated" event
 channel.bind("OrderUpdated", (data: any) => {
   console.log("Order updated:", data);
 });
 channel.bind("OrderCreated", (data: any) => {
-  console.log("Order updated:", data);
+  console.log("Order created:", data);
 });
 
 export default pusher;
