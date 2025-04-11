87d4d07568814b5c72b7b4d8055d1128ce38e0d8
sefin-cc <rogenasefin6@gmail.com>
2025-04-11 20:00:48 +0800

    Update broadcasting.php

--- server/config/broadcasting.php
+++ server/config/broadcasting.php
@@ -41,6 +41,7 @@ return [
                 'port' => env('REVERB_PORT', 443),
                 'scheme' => env('REVERB_SCHEME', 'https'),
                 'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                'allowed_origins' => explode(',', env('REVERB_ALLOWED_ORIGINS', '')),
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
